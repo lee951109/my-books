90925c74caff8f6a071529a13b59b8bf7bef71c1
lee951109 <sk985326@naver.com>
2022-08-08 22:19:13 +0900

    my-books start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add";
+import Detail from "./pages/Detail";
+import Edit from "./pages/Edit";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Error from "./pages/Error";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* 런타임에 문제가 생길시 Error page가 보여진다.
+          npm i react-error-boundary */}
+      <ErrorBoundary FallbackComponent={Error}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/add" element={<Add />} />;
+          <Route path="/book/:id" element={<Detail />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ErrorBoundary>
     </div>
   );
 }
